0aac738a578c5bc23c70eb583c8d8c6923771ed8
Sebastian Andreasson <sebastian.andreasson93@gmail.com>
2025-11-10 11:45:56 +0100

    attach console

--- Program.cs
+++ Program.cs
@@ -1,19 +1,42 @@
 using System;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
 
 namespace GlobalTextHelper
 {
     internal static class Program
     {
+        private const uint ATTACH_PARENT_PROCESS = 0xFFFFFFFF;
+
+        [DllImport("kernel32.dll")] static extern bool AttachConsole(uint dwProcessId);
+        [DllImport("kernel32.dll")] static extern bool AllocConsole();
+        [DllImport("kernel32.dll")] static extern bool FreeConsole();
+
         [STAThread]
         static void Main()
         {
+            // Try to attach to the parent console (e.g., the one you ran `dotnet run` from).
+            if (!AttachConsole(ATTACH_PARENT_PROCESS))
+            {
+                // If there isn't one (e.g., started from Explorer), make a new console.
+                AllocConsole();
+            }
+
+            // optional: so UTF-8 glyphs print nicely
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
             ApplicationConfiguration.Initialize();
+
             using var activeWindowMonitor = new ActiveWindowMonitor();
             activeWindowMonitor.Start();
 
-            Application.ApplicationExit += (_, __) => activeWindowMonitor.Stop();
+            Application.ApplicationExit += (_, __) =>
+            {
+                activeWindowMonitor.Stop();
+                FreeConsole(); // close the console on app exit
+            };
 
+            Console.WriteLine("GlobalTextHelper started. Listening for foreground window changes...");
             Application.Run(new MainForm());
         }
     }
